3887c7120b14985d95d903867274e5385038a277
JacquesYW <1263264500@qq.com>
2018-08-08 14:42:30 +0800

    ip切换

--- src/default.js
+++ src/default.js
@@ -1,6 +1,6 @@
 export default {
     //地图服务地址ip
-    mapServerURL: 'http://120.26.218.105:25048/mapplugin',
+    mapServerURL: 'http://120.26.217.62:25048/mapplugin',
     //arcgis地图服务ip
-    arcgisServerURL: 'http://120.26.218.105:25048/gis'
+    arcgisServerURL: 'http://120.26.217.62:25048/gis'
 }
