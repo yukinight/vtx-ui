634acdd6228017fc560ab01fc5307e4de1fcf692
TianQi Zhang <jsxsztq@163.com>
2018-12-25 17:32:49 +0800

    弹窗demo修改

--- demo/src/routes/modal.js
+++ demo/src/routes/modal.js
@@ -3,32 +3,44 @@ import { connect } from 'dva';
 import styles from './modal.less';
 
 import {VtxModal} from 'vtx-ui';
-
+const {DraggableModal} = VtxModal;
 import {Button,Input} from 'antd';
 
 function IndexPage({dispatch,vtxModal}) {
-    const {visible} = vtxModal;
-    function showModal(boolean) {
+    const {visible, visible2} = vtxModal;
+    function updateState(obj) {
         dispatch({
             type: 'vtxModal/updateState',
-            payload: {
-                visible: boolean
-            }
+            payload: obj
         })
     }
     return (
         <div className={styles.normal}>
-            <Button className={styles.btn} onClick={()=>showModal(true)}>弹窗</Button>
+            <Button className={styles.btn} onClick={()=>updateState({visible:true})}>弹窗</Button>
+            <Button className={styles.btn} onClick={()=>updateState({visible2:true})}>弹窗2</Button>
+
             <VtxModal 
                 title={'adfasfd'}
                 visible={visible}
                 wrapClassName={'adhfas'}
-                onCancel={()=>showModal(false)}
+                onCancel={()=>updateState({visible:false})}
             >
                 <Input 
                     onPressEnter={()=>{console.log(1111)}}
                 />
             </VtxModal>
+
+            <DraggableModal 
+                key='movemd'
+                title={'可拖动的弹框'}
+                visible={visible2}
+                wrapClassName={'adhfas'}
+                onCancel={()=>updateState({visible2:false})}
+            >
+                <Input 
+                    onPressEnter={()=>{console.log(1111)}}
+                />
+            </DraggableModal>
         </div>
     );
 }
