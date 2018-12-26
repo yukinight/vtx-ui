4a05a3f3dbd99c7e74ee1a534adbd46df9515ce0
TianQi Zhang <jsxsztq@163.com>
2018-12-26 16:02:47 +0800

    demo增加导航页

--- demo/src/routes/IndexPage.js
+++ demo/src/routes/IndexPage.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import { connect } from 'dva';
 import styles from './IndexPage.css';
+import {Link} from 'dva/router';
 
 function IndexPage() {
 
-  return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to vtx-ui!</h1>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
-        <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
-      </ul>
-    </div>
-  );
+    return (
+        <div className={styles.normal}>
+            <ul>
+                <li><Link to='/tree'>树</Link></li>
+                <li><Link to='/ztree'>高性能树</Link></li>
+                <li><Link to='/treeselect'>下拉树</Link></li>
+                <li><Link to='/datagrid'>表格</Link></li>
+                <li><Link to='/map'>地图</Link></li>
+                <li><Link to='/optmap'>网格地图(点位过滤)</Link></li>
+                <li><Link to='/searchmap'>弹框地图</Link></li>
+                <li><Link to='/date'>日期控件</Link></li>
+                <li><Link to='/grid'>CRUD头部查询条件</Link></li>
+                <li><Link to='/combogrid'>下拉表格</Link></li>
+                <li><Link to='/modal'>弹框</Link></li>
+                <li><Link to='/upload'>上传</Link></li>
+                <li><Link to='/import'>导入</Link></li>
+                <li><Link to='/export'>导出</Link></li>
+            </ul>
+        </div>
+    );
 }
 
 export default connect()(IndexPage);
