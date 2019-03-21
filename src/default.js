d9bfe00d308d7b2c4a8e5dc4a0dd740188a76786
manwuyu <xuzhengyun1989@163.com>
2019-03-21 11:28:34 +0800

    [misc]曹飞配置好https协议需用域名访问

--- src/default.js
+++ src/default.js
@@ -1,8 +1,8 @@
 export default {
     //地图服务地址ip
-    mapServerURL: '//120.26.217.62:25048/mapplugin',
+    mapServerURL: '//vortexplugin.cloudhw.cn/mapplugin',
     //arcgis地图服务ip
-    arcgisServerURL: '//120.26.217.62:25048/gis',
+    arcgisServerURL: '//vortexplugin.cloudhw.cn/gis',
     // ztree文件地址
-    ztreeServer:'//120.26.217.62:25048/ztree',
+    ztreeServer:'//vortexplugin.cloudhw.cn/ztree',
 }
