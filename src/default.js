ce71afc428c2844feb65059241cb380187a8fde9
manwuyu <xuzhengyun1989@163.com>
2019-03-21 11:12:19 +0800

    支持http和https协议

--- src/default.js
+++ src/default.js
@@ -1,8 +1,8 @@
 export default {
     //地图服务地址ip
-    mapServerURL: 'https://120.26.217.62:25048/mapplugin',
+    mapServerURL: '//120.26.217.62:25048/mapplugin',
     //arcgis地图服务ip
-    arcgisServerURL: 'https://120.26.217.62:25048/gis',
+    arcgisServerURL: '//120.26.217.62:25048/gis',
     // ztree文件地址
-    ztreeServer:'https://120.26.217.62:25048/ztree',
+    ztreeServer:'//120.26.217.62:25048/ztree',
 }
