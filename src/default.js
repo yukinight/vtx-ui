62f351098ce637ef27eabdaf6b2a99b150c14043
JacquesYW <1263264500@qq.com>
2019-04-25 09:49:55 +0800

    1.漏提交的 map https的配置

--- src/default.js
+++ src/default.js
@@ -1,4 +1,5 @@
 export default {
+    httpOrhttps: (window.VtxPublicServiceAddress || {}).httpOrhttps || 'http',
     //地图服务地址ip
     mapServerURL:  (window.VtxPublicServiceAddress || {}).mapServerURL || 'http://120.26.217.62:25048/mapplugin',
     //arcgis地图服务ip
