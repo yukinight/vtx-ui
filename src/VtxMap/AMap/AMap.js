39cea0c3aef01dfe34627cceb974d4a75078bea2
JacquesYW <1263264500@qq.com>
2019-07-30 11:57:23 +0800

    1.高德地图 引入 Geolocation 定位插件

--- src/VtxMap/AMap/AMap.js
+++ src/VtxMap/AMap/AMap.js
@@ -65,7 +65,7 @@ class VortexAMap extends React.Component{
             }
             else{
                 $.getScript(`${configUrl.mapServerURL}/A_content.js`,()=>{
-                    $.getScript(`${configUrl.httpOrhttps}://webapi.amap.com/maps?v=1.4.14&key=e59ef9272e3788ac59d9a22f0f8cf9fe&plugin=AMap.MarkerClusterer,AMap.Scale,AMap.ToolBar,AMap.DistrictSearch,AMap.RangingTool,AMap.MouseTool,AMap.PolyEditor,AMap.CircleEditor,AMap.PlaceSearch,AMap.Heatmap`,()=>{
+                    $.getScript(`${configUrl.httpOrhttps}://webapi.amap.com/maps?v=1.4.14&key=e59ef9272e3788ac59d9a22f0f8cf9fe&plugin=AMap.Geolocation,AMap.MarkerClusterer,AMap.Scale,AMap.ToolBar,AMap.DistrictSearch,AMap.RangingTool,AMap.MouseTool,AMap.PolyEditor,AMap.CircleEditor,AMap.PlaceSearch,AMap.Heatmap`,()=>{
                         let PointCollection = new Promise((resolve,reject)=>{
                             $.getScript(`${configUrl.mapServerURL}/GPointCollection.js`,()=>{
                                 resolve();
