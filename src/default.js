aab25b59c46ad1b746af54a80edc1278b64637f3
JacquesYW <1263264500@qq.com>
2019-11-12 16:29:53 +0800

    1.修改js服务地址,默认适应https方式

--- src/default.js
+++ src/default.js
@@ -1,9 +1,18 @@
 export default {
     httpOrhttps: (window.VtxPublicServiceAddress || {}).httpOrhttps || 'http',
     //地图服务地址ip
-    mapServerURL:  (window.VtxPublicServiceAddress || {}).mapServerURL || 'http://120.26.217.62:25048/mapplugin',
+    mapServerURL: (window.VtxPublicServiceAddress || {}).mapServerURL ?
+        (window.VtxPublicServiceAddress || {}).mapServerURL:
+            (window.VtxPublicServiceAddress || {}).httpOrhttps == 'https'?
+                'https://vortexplugin.cloudhw.cn/mapplugin' : 'http://120.26.217.62:25048/mapplugin',
     //arcgis地图服务ip
-    arcgisServerURL:  (window.VtxPublicServiceAddress || {}).arcgisServerURL || 'http://120.26.217.62:25048/gis',
+    arcgisServerURL:  (window.VtxPublicServiceAddress || {}).arcgisServerURL ?
+        (window.VtxPublicServiceAddress || {}).arcgisServerURL:
+            (window.VtxPublicServiceAddress || {}).httpOrhttps == 'https'?
+                'https://vortexplugin.cloudhw.cn/gis' : 'http://120.26.217.62:25048/gis',
     // ztree文件地址
-    ztreeServer: (window.VtxPublicServiceAddress || {}).ztreeServer || 'http://120.26.217.62:25048/ztree',
+    ztreeServer: (window.VtxPublicServiceAddress || {}).ztreeServer ?
+        (window.VtxPublicServiceAddress || {}).ztreeServer:
+            (window.VtxPublicServiceAddress || {}).httpOrhttps == 'https'?
+            'https://vortexplugin.cloudhw.cn/ztree' : 'http://120.26.217.62:25048/ztree',
 }
